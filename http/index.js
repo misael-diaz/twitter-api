9ed2e0e8e7e5b05c35769a4a7805932e43e08ba8
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-02-17 17:19:18 -0400

    exports start() method for starting the HTTP App
    COMMENTS:
    Initial import.
    
    This commit borrows from https://github.com/jestrade/api-twitter.
    
    The app listens on port 8080 and answers GET requests at root `/'.

---
+++ http/index.js
@@ -0,0 +1,72 @@
+/*
+
+Twitter API							February 17, 2023
+
+source: index.js
+author: @misael-diaz
+
+Synopsis:
+Exports HTTP App start() method.
+
+
+Copyright (c) 2023 Misael Diaz-Maldonado
+This file is released under the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+
+References:
+[0] main resource: https://github.com/jestrade/api-twitter
+[1] API: https://www.ibm.com/topics/api
+[2] NODEJS REST API: https://medium.com/bb-tutorials-and-thoughts/how-to-write-production-ready-node-js-rest-api-javascript-version-db64d3941106
+[3] app.use(): https://www.geeksforgeeks.org/express-js-app-use-function/
+[4] express.json(): https://www.geeksforgeeks.org/express-js-express-json-function/
+[5] app.get(): https://www.geeksforgeeks.org/express-js-app-get-request-function/
+
+*/
+
+
+// imports dependencies:
+
+
+const express = require("express");	// imports express
+const { http } = require("../config");	// gets http object from config by deconstruction
+const { port } = http;			// gets service port from http by deconstruction
+
+
+// instantiates the HTTP App:
+
+
+const app = express();
+
+
+// mounts middleware:
+
+
+app.use(express.json());		// mounts middleware for parsing json requests
+
+
+// configures the HTTP App:
+
+
+// configures the app to answer HTTP GET request to the root route `/':
+app.get("/", (req, res) => {
+	res.send("App works!");
+});
+
+
+// configures the app to handle undefined HTTP GET routes:
+app.get("*", (req, res) => {
+	res.send("undefined route");
+});
+
+
+// defines the start() method for starting the HTTP App:
+const start = () => {
+	app.listen(port, () => {
+		console.log(`server running at http://localhost:${port}`);
+	});
+};
+
+
+module.exports = { start };		// exports object storing the start() method
