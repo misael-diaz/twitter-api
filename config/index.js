73fd5a26c19b43c572df07229c2b70d597f3f021
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-03-27 11:33:53 -0400

    relocates general info [NodeJS]

--- config/index.js
+++ config/index.js
@@ -1,24 +1,3 @@
-/*
-
-Twitter API							February 17, 2023
-
-source: index.js
-author: @misael-diaz
-
-Synopsis:
-Configures the HTTP Host and Port of the App.
-
-
-Copyright (c) 2023 Misael Diaz-Maldonado
-This file is released under the GNU General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-
-References:
-[0] main: https://github.com/jestrade/api-twitter
-
-*/
 
 
 // imports dependencies:
@@ -48,3 +27,25 @@ const config = {
 
 
 module.exports = config;		// exports the configuration object `config'
+
+/*
+
+Twitter API							February 17, 2023
+
+source: index.js
+author: @misael-diaz
+
+Synopsis:
+Configures the HTTP Host and Port of the App.
+
+
+Copyright (c) 2023 Misael Diaz-Maldonado
+This file is released under the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+
+References:
+[0] main: https://github.com/jestrade/api-twitter
+
+*/
