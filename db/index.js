49dd0411ee6569396dcd74a969b956c8c644f206
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-03-30 13:28:04 -0400

    adds database (dis)connection methods [NodeJS]

---
+++ db/index.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const { db } = require("../config");
+const { connectionString } = db;
+
+const connect = async () => {
+
+  const uri = connectionString;
+  const opt = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  };
+
+  try
+  {
+    await mongoose.connect(uri, opt);
+    console.log(`successful connection to the database!`);
+  }
+  catch (e)
+  {
+    console.error(`DataBaseError code: ${e.code} info: ${e.message}`);
+  }
+
+};
+
+const disconnect = () => {
+  mongoose.connection.close();
+}
+
+module.exports = { connect, disconnect };
+
+/*
+
+Twitter API							February 09, 2023
+
+source: index.js
+author: @misael-diaz
+
+Synopsis:
+Exports methods for (dis)connecting to the database.
+
+
+Copyright (c) 2023 Misael Diaz-Maldonado
+This file is released under the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+
+References:
+[0] https://github.com/jestrade/api-twitter
+
+*/
