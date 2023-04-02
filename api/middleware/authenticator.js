0e916361ce1cbe92e5e346afd65fa31840b1a21c
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-04-02 11:50:21 -0400

    changes indentation

--- api/middleware/authenticator.js
+++ api/middleware/authenticator.js
@@ -3,35 +3,34 @@ const config = require("../../config");	// for the header and secret token keys
 
 const { token } = config.jwt;
 
-
 const authenticator = (req, res, next) => {
-// authenticates user by verifying the token sent in the request header
+  // authenticates user by verifying the token sent in the request header
 
-	try
-	{
-		// gets the token header-key from the request header (ref [2])
-		const t = req.header(token.headerKey);
+  try
+  {
+    // gets the token header-key from the request header (ref [2])
+    const t = req.header(token.headerKey);
 
-		// Note:
-		// Decodes the data in token if the token was signed with the private
-		// token key of the twitter-api (defined in .env).
-		const data = jwt.verify(t, token.secretKey);
+    // Note:
+    // Decodes the data in token if the token was signed with the private
+    // token key of the twitter-api (defined in .env).
+    const data = jwt.verify(t, token.secretKey);
 
-		if (data)
-		// gets the userID and adds it to the request body for next()
-		{
-			const { userID } = data;
-			req.body.userID = userID;
-			console.log(`token has been decoded successfully!`);
-		}
+    if (data)
+    // gets the userID and adds it to the request body for next()
+    {
+      const { userID } = data;
+      req.body.userID = userID;
+      console.log(`token has been decoded successfully!`);
+    }
 
-		next();
+    next();
 
-	}
-	catch (error)
-	{
-		res.status(401).send(error);
-	}
+  }
+  catch (error)
+  {
+    res.status(401).send(error);
+  }
 };
 
 
